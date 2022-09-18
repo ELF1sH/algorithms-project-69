477924a47bec1903dc5a1f297d3b5385a52d43a0
Igor <86882588+Ray-Janson@users.noreply.github.com>
2022-09-19 01:54:26 +0700

    Stage 2

--- __tests__/buildSearchEngine.test.js
+++ __tests__/buildSearchEngine.test.js
@@ -1,12 +1,18 @@
 import { test, expect } from "@jest/globals";
 import buildSearchEngine from "../src/BuildSearchEngine.js";
 
-const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
-const doc3 = { id: 'doc3', text: "I'm your shooter." };
-const docs = [doc1, doc2, doc3];
-const searchEngine = buildSearchEngine(docs);
-
-test('contains 1', () => {
+test('Stage1:test1', () => {
+  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+  const doc3 = { id: 'doc3', text: "I'm your shooter." };
+  const docs = [doc1, doc2, doc3];
+  const searchEngine = buildSearchEngine(docs);
   expect(searchEngine.search('shoot')).toStrictEqual(['doc1', 'doc2']);
 });
+
+test('Stage2:test1', () => {
+  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+  const docs = [doc1];
+  const searchEngine = buildSearchEngine(docs);
+  expect(searchEngine.search('pint!')).toStrictEqual(['doc1']);
+})
--- src/BuildSearchEngine.js
+++ src/BuildSearchEngine.js
@@ -1,8 +1,17 @@
+const getRidOfSymbols = (text) => {
+  const regExp = new RegExp(`(!)|(\\?)|(,)|(\\.)|(\\()|(\\))|(-)|(')|(")|(\\^)|(_)`, 'g');
+  return text.replaceAll(regExp, '');
+}
+
 const buildSearchEngine = (docs) => {
   return {
-    docs,
+    docs: docs.map(doc => ({...doc, text: getRidOfSymbols(doc.text)})),
     search(word) {
-      return this.docs.filter((doc) => doc.text.split(' ').includes(word)).map(doc => doc.id);
+      const fixedWord = getRidOfSymbols(word);
+      return this.docs
+        .filter((doc) => doc.text.split(' ')
+        .includes(fixedWord))
+        .map(doc => doc.id);
     },
   };
 };
