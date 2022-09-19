23f2c58f727b58a9fe5a9ecabdc1996c47c017d0
Igor <86882588+Ray-Janson@users.noreply.github.com>
2022-09-19 23:37:13 +0700

    Improved code quality v3

--- src/BuildSearchEngine.js
+++ src/BuildSearchEngine.js
@@ -24,14 +24,31 @@ const getInvertedIndex = (docs) => {
       doc.text.includes(word) ? [...acc, doc.id] : acc
     ), []),
   }), {});
-}
+};
 
-const getTF = (docText, targetWordRegExp) => {
+const calculateTF = (docText, targetWordRegExp) => {
   const targetWordsInText = (docText.match(targetWordRegExp) || []).length;
   const wordsInText = docText.match(WORD_REGEXP).length;
   return targetWordsInText / wordsInText;
-}
+};
 
+const calculateIDF = (docsNumber, invertedIndex, targetWord) => {
+  let currentIDF = docsNumber / (invertedIndex[targetWord] ? invertedIndex[targetWord].length : 0);
+  if (!isFinite(currentIDF)) {
+    currentIDF = 0;
+  }
+  return currentIDF;
+};
+
+const getTF = (TFs, targetWord, docId) => (
+  TFs[targetWord].find((value) => Object.keys(value)[0] === docId)[docId]
+);
+
+const findSumTFIDFbyDocId = (TFIDF, docId) => (
+  Object.values(TFIDF).reduce((acc, targetWordDocs) => (
+    acc + targetWordDocs.find((value) => Object.keys(value)[0] === docId)[docId]
+  ), 0)
+);
 // ------------------------
 // functions-helpers block *ENDING*
 // ------------------------
@@ -58,15 +75,14 @@ const buildSearchEngine = (docs) => {
         const regExp = new RegExp(`(?<![a-zA-Z])${targetWord}(?![a-zA-Z])`, 'g');
         return {
           ...acc,
-          [targetWord]: normalizedDocs.reduce((acc, doc) => [...acc, { [doc.id]: getTF(doc.text, regExp) }], []),
+          [targetWord]: normalizedDocs.reduce((acc, doc) => (
+            [...acc, { [doc.id]: calculateTF(doc.text, regExp) }]
+          ), []),
         }
       }, {});
 
       const IDF = normalizedTargetArray.reduce((acc, targetWord) => {
-        let currentIDF = docs.length / (invertedIndex[targetWord] ? invertedIndex[targetWord].length : 0);
-        if (!isFinite(currentIDF)) {
-          currentIDF = 0;
-        }
+        let currentIDF = calculateIDF(docs.length, invertedIndex, targetWord);
         return {
           ...acc,
           [targetWord]: Math.log(1 + currentIDF),
@@ -76,24 +92,22 @@ const buildSearchEngine = (docs) => {
       const TFIDF = normalizedTargetArray.reduce((acc, targetWord) => {
         return {
           ...acc,
-          [targetWord]: normalizedDocs.reduce((acc, doc) => [...acc, { [doc.id]: TFs[targetWord].find(value => Object.keys(value)[0] === doc.id)[doc.id] * IDF[targetWord] }], []),
+          [targetWord]: normalizedDocs.reduce((acc, doc) => (
+            [...acc, { [doc.id]: getTF(TFs, targetWord, doc.id) * IDF[targetWord] }]
+          ), []),
         }
       }, {});
 
-      const res = normalizedDocs.reduce((acc, doc) => {
-        return [
-          ...acc,
-          [doc.id, Object.values(TFIDF).reduce((acc, targetWord) => acc + targetWord.find(value => Object.keys(value)[0] === doc.id)[doc.id], 0)],
-        ]
-      }, []);
+      const res = normalizedDocs.reduce((acc, doc) => (
+        [...acc, [doc.id, findSumTFIDFbyDocId(TFIDF, doc.id)]]
+      ), []);
 
       return res
-        .filter((item) => item[1] > 0)
-        .sort((a, b) => b[1] - a[1])
-        .map((item) => item[0])
+        .filter((item) => item[1] > 0)  // sum bigger than 0
+        .sort((a, b) => b[1] - a[1])    // sort by desc
+        .map((item) => item[0]);        // get rid of sums leaving only IDs
     },
   };
-
 };
 
 export default buildSearchEngine;
